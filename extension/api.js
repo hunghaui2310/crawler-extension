b4b58157243619a8b6a537bb7682fc796d04b6c7
Hungnn <hung.nguyen@dabeeovina.com>
2024-03-15 16:20:25 +0700

    add API to FE

--- extension/api.js
+++ extension/api.js
@@ -1,12 +1,16 @@
 const API_URL = 'http://localhost:8080/api';
+const BASE_HEADER = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+const getAllShopId = () => fetch(API_URL + "/shop", {
+    method: 'GET',
+}).then(response => response.json())
 
 
 const saveRawItem = (data, url) => fetch(API_URL + "/shop-raw", {
     method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
+    headers: BASE_HEADER,
     body: JSON.stringify({ url, data })
-})
-  .then(response => response.json())
+}).then(response => response.json())
