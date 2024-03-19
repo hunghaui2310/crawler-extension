fa361467c3bb7db86efc66f3ce259d31b29c84bb
chris.nguyen <kiennt.6795@gmail.com>
2024-03-19 17:06:23 +0700

    step 3 + update step 2

--- extension/crawl-shop-items.js
+++ extension/crawl-shop-items.js
@@ -2,6 +2,7 @@ let PAGE_SHOP = 0;
 let currentShopId;
 let tempShopId;
 const SHOP_IDS = 'SHOP_IDS';
+const SHOP_CRAWLED_IDS = 'SHOP_CRAWLED_IDS';
 const localStorageManager = new LocalStorageManager();
 let urlShop;
 
@@ -20,11 +21,20 @@ window.addEventListener('getItemsList', async (event) => {
     if (!shopIds || shopIds.length === 0) {
         await statusRawShopAPI(true);
         await statusRawItemAPI(true);
+        step = null;
     }
     if (shopIds.length) {
         PAGE_SHOP = 0;
         const [firstShop, ...shops] = shopIds;
         currentShopId = firstShop;
+        let shop_crawled;
+        if (!localStorageManager.getItem(SHOP_CRAWLED_IDS)) {
+            shop_crawled = [currentShopId, ...localStorageManager.getItem(SHOP_CRAWLED_IDS)];
+        } else {
+            shop_crawled = [currentShopId];
+        }
+        localStorageManager.removeItem(SHOP_CRAWLED_IDS);
+        localStorageManager.setItem(SHOP_CRAWLED_IDS, shop_crawled);
         localStorageManager.removeItem(SHOP_IDS);
         localStorageManager.setItem(SHOP_IDS, shops);
         crawlShopItems(currentShopId);
@@ -68,75 +78,81 @@ function saveShop(data, phone, address) {
 }
 
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
-  if (request.request.url && request.request.url.includes("get_shop_base")) {
-    request.getContent((content, mimeType) => {
-      // console.log('content', content);
-      // console.log('phone ' + getPhone(content));
-      if (tempShopId !== currentShopId) {
-        // console.log('get_shop_base', content);
-        //TODO: bóc tách dữ liệu lấy SĐT, địa chỉ từ raw content (text)
-        const { data } = JSON.parse(content);
-        currentPhone = getPhone(content);
-        currentAddress = getAddress(content);
-        saveShop(data, currentPhone, currentAddress);
+  if(step === 2) {
+    if (request.request.url && request.request.url.includes("get_shop_base")) {
+        request.getContent((content, mimeType) => {
+          // console.log('content', content);
+          // console.log('phone ' + getPhone(content));
+          if (tempShopId !== currentShopId) {
+            // console.log('get_shop_base', content);
+            //TODO: bóc tách dữ liệu lấy SĐT, địa chỉ từ raw content (text)
+            const { data } = JSON.parse(content);
+            const phone = getPhone(data.description);
+            const address = getAddress(data.description);
+            const out = {
+                shopid: data.shopid,
+                username: data.account.username,
+                shopName: data.name,
+                description: data.description
+            };
+            console.log(out);
+            saveRawShopAPI(JSON.stringify(out), urlShop);
+            updateShopAPI(
+                {  
+                    shopid: currentShopId, 
+                    rawInfo: content, 
+                    detailAddress: address, 
+                    detailPhone: phone
+                }
+            )
+          }
+        });
       }
-    });
-  }
-  if (request.request.url && request.request.url.includes("get_shop_tab")) {
-    request.getContent((content, mimeType) => {
-      // console.log('content', content);
-
-      if (tempShopId !== currentShopId) {
-        const { data } = JSON.parse(content);
-        console.log("phone: " + getPhone(content));
-        console.log("address: " + getAddress(content));
-        const phone = getPhone(content);
-        const address = getAddress(content);
-        let isUpdate = false;
-        if (phone && phone !== currentPhone) {
-          currentPhone = phone;
-          isUpdate = true;
+      if (request.request.url && request.request.url.includes("get_shop_tab")) {
+        request.getContent((content, mimeType) => {
+          // console.log('content', content);
+    
+          if (tempShopId !== currentShopId) {
+            const { data } = JSON.parse(content);
+            // console.log("phone: " + getPhone(content));
+            // console.log("address: " + getAddress(content));
+            const phone = getPhone(content);
+            const address = getAddress(content);
+            if (phone || address) {
+                updateShopAPI(
+                    {  
+                        shopid: currentShopId, 
+                        rawInfo: content, 
+                        detailAddress: address, 
+                        detailPhone: phone
+                    }
+                )
+            }
+          }
+        });
+      }
+      if (request.request.url && request.request.url.includes("shop/rcmd_items")) {
+        request.getContent((content, mimeType) => {
+          const { data } = JSON.parse(content);
+          if (!data.items || (data.items && data.items.length === 0 )) {
+            window.dispatchEvent(
+                new CustomEvent(
+                    'getItemsList'
+                )
+            )
+            return;
         }
-        if (address && address !== currentAddress) {
-          currentAddress = address;
-          isUpdate = true;
-        }
-        if (isUpdate) {
-          saveShop(data, currentPhone, currentAddress);
-        }
+          saveRawProductAPI(JSON.stringify(data.items), urlShop);
+          setTimeout(() => {
+            tempShopId = currentShopId;
+            if ((PAGE_SHOP + 1) * 30 > data.total) {
+              window.dispatchEvent(new CustomEvent("getItemsList"));
+            } else {
+              window.dispatchEvent(new CustomEvent("callLoopPageShop"));
+            }
+          }, getRandomTime());
+        });
       }
-    });
-  }
-  if (request.request.url && request.request.url.includes("shop/rcmd_items")) {
-    request.getContent((content, mimeType) => {
-      const { data } = JSON.parse(content);
-      console.log("phone: " + getPhone(content));
-      console.log("address: " + getAddress(content));
-      const phone = getPhone(content);
-      const address = getAddress(content);
-      let isUpdate = false;
-      if (phone && phone !== currentPhone) {
-        currentPhone = phone;
-        isUpdate = true;
-      }
-      if (address && address !== currentAddress) {
-        currentAddress = address;
-        isUpdate = true;
-      }
-      if (isUpdate) {
-        saveShop(data, currentPhone, currentAddress);
-      }
-      //TODO: push data shop to BE
-      saveRawProductAPI(JSON.stringify(data.items), urlShop);
-      setTimeout(() => {
-        tempShopId = currentShopId;
-        if ((PAGE_SHOP + 1) * 30 > data.total) {
-          window.dispatchEvent(new CustomEvent("getItemsList"));
-        } else {
-          window.dispatchEvent(new CustomEvent("callLoopPageShop"));
-        }
-      }, getRandomTime());
-    });
   }
 });
 
@@ -148,5 +164,6 @@ document.getElementById("crawl-items-shop").addEventListener("click", () => {
       },
     })
   );
+  step = 2;
   document.getElementById("crawl-items-shop").disabled = true;
 });
---
+++ extension/detail-item.js
@@ -0,0 +1,143 @@
+let currentShopIdItemDetail;
+let listItemsOfShop = [];
+const localStorageManagerItem = new LocalStorageManager();
+let urlItemDetail;
+
+window.addEventListener('getListShopCrawled', async (event) => {
+    // let response = await getAllShopIdAPI();
+    let response = [
+        // 727529781,
+        // 884804991,
+        // 531199972,
+        529573640,
+        1072321433,
+        // 530152797,
+        // 967732774,
+        // 620506764,
+        // 529455673,
+        // 716766179,
+        // 533323302,
+        // 629627436,
+        // 728332247,
+        // 673416314,
+        // 864465563,
+        // 842067593,
+        // 134741968,
+        // 716808781,
+        // 537291716,
+        // 716709822
+    ];
+    localStorageManagerItem.setItem(SHOP_CRAWLED_IDS, response);
+    window.dispatchEvent(
+        new CustomEvent(
+            'getItemsListInShop'
+        )
+    )
+});
+
+window.addEventListener('getItemsListInShop', async (event) => {
+    let shopIds = localStorageManagerItem.getItem(SHOP_CRAWLED_IDS);
+    if (shopIds.length) {
+        const [firstShop, ...shops] = shopIds;
+        currentShopIdItemDetail = firstShop;
+        localStorageManager.removeItem(SHOP_CRAWLED_IDS);
+        localStorageManager.setItem(SHOP_CRAWLED_IDS, shops);
+        listItemsOfShop = await getProductByShopAPI(currentShopIdItemDetail);
+        window.dispatchEvent(
+            new CustomEvent(
+                'getDetailItem'
+            )
+        )
+    }
+});
+
+window.addEventListener('getDetailItem', async (event) => {
+    if (listItemsOfShop.length) {
+        const [firstItem, ...items] = listItemsOfShop;
+        listItemsOfShop = items;
+        crawlItem(firstItem);
+    }
+});
+
+function buildUrlItem(item) {
+    urlShop = `https://shopee.vn/product/${item.shopid}/${item.itemid}`;
+    return urlShop
+}
+
+async function crawlItem(item) {
+    const url = buildUrlItem(item);
+    routeToPage(url);
+}
+
+function getRandomTime() {
+    const arrayTime = [3000, 5000, 6000];
+    const randomIndex = Math.floor(Math.random() * arrayTime.length);
+    return arrayTime[randomIndex];
+}
+
+chrome.devtools.network.onRequestFinished.addListener(
+    function(request) {
+        if (request.request.url && request.request.url.includes('/get_pc')) {
+            request.getContent((content, mimeType) => {
+                const {data} = JSON.parse(content);
+                if (data && data.item && data.item.description) {
+                    const phone = getPhone(data.item.description);
+                    const address = getAddress(data.item.description);
+                    console.log('phone', phone);
+                    console.log('address', address);
+                    updateShopAPI(
+                        {  
+                            shopid: currentShopIdItemDetail, 
+                            rawInfo: data.item.description, 
+                            detailAddress: address, 
+                            detailPhone: phone
+                        }
+                    );
+                }
+            });
+        }
+        if (request.request.url && request.request.url.includes('get_ratings')) {
+            request.getContent((content, mimeType) => {
+                const phone = getPhone(content);
+                const address = getAddress(content);
+                updateShopAPI(
+                    {  
+                        shopid: currentShopIdItemDetail, 
+                        rawInfo: content, 
+                        detailAddress: address, 
+                        detailPhone: phone
+                    }
+                );
+                setTimeout(() => {
+                    if (listItemsOfShop.length) {
+                        window.dispatchEvent(
+                            new CustomEvent(
+                                'getDetailItem'
+                            )
+                        )
+                    } else {
+                        window.dispatchEvent(
+                            new CustomEvent(
+                                'getItemsListInShop'
+                            )
+                        )
+                    }
+                }, getRandomTime())
+            });
+        }
+    }
+);
+
+document.getElementById('crawl-detail-item').addEventListener('click', () => {
+    window.dispatchEvent(new CustomEvent(
+            'getListShopCrawled',
+            {
+                detail: {
+                    status: 'start',
+                }
+            }
+        )
+    );
+    document.getElementById('crawl-detail-item').disabled = true;
+});
+
