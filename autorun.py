01bcae851a7288efbd8d356492b668dce531652c
Hungnn <hung.nguyen@dabeeovina.com>
2024-03-30 11:18:47 +0700

    fix

--- autorun.py
+++ autorun.py
@@ -1,9 +1,43 @@
 import pyautogui
 import pygetwindow as gw
+import time
+import platform
+import subprocess
+
+urlShopee = 'https://shopee.vn/buyer/login'
+jscode = "document.querySelector('input[name=\"loginKey\"]').value = '03456789';"
 
 def is_chrome_focused():
     chrome_windows = gw.getActiveWindow()
     return chrome_windows.strip() == 'Google Chrome'
+
+def open_chrome_incognito(url):
+    system = platform.system()
+    if system == 'Windows':
+        command = 'chrome.exe --incognito'
+    elif system == 'Darwin':  # macOS
+        command = 'open -na "Google Chrome" --args --incognito'
+    elif system == 'Linux':
+        command = 'google-chrome --incognito'
+    else:
+        print("Unsupported operating system")
+        return
+    
+    try:
+        subprocess.run(command + ' ' + url, shell=True)
+    except Exception as e:
+        print("Error when open Google Incognito:", e)
+
+def close_chrome_tab():
+    system = platform.system()
+    if system == 'Windows':
+        subprocess.run('taskkill /f /im chrome.exe', shell=True)
+    elif system == 'Darwin':  # macOS
+        subprocess.run('osascript -e \'quit app "Google Chrome"\'', shell=True)
+    elif system == 'Linux':
+        subprocess.run('pkill chrome', shell=True)
+    else:
+        print("Unsupported operating system")
 
 def auto_open_console_and_nav_extension():
     pyautogui.keyDown('command')
@@ -19,13 +53,30 @@ def auto_open_console_and_nav_extension():
     # Release Cmd key
     pyautogui.keyUp('command')
 
-    for x in range(12):
-        pyautogui.keyDown('command')
-        pyautogui.keyDown(']')
-        pyautogui.keyUp(']')
-        pyautogui.keyUp('command')
+    pyautogui.keyDown('command')
+    # time.sleep(1)
+    for x in range(2):
+        pyautogui.keyDown('[')
+        time.sleep(0.5)
+        pyautogui.keyUp('[')
+    pyautogui.keyUp('command')
 
-if is_chrome_focused():
+def auto_login(username, password):    
+    # enter username
+    pyautogui.typewrite(username)
+    # enter password
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.typewrite(password)
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.press('enter')
     auto_open_console_and_nav_extension()
+    
+open_chrome_incognito(urlShopee)
+time.sleep(3)
+if is_chrome_focused():
+    auto_login('dungzboo@gmail.com', 'Vietdungzykh')
 else:
     print("Chrome window is not focused.")
--- crawler/src/main/java/com/hk/max/CapApplication.java
+++ crawler/src/main/java/com/hk/max/CapApplication.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -13,10 +14,9 @@ import org.springframework.context.event.EventListener;
 
 import org.openqa.selenium.WebDriver;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 @Slf4j
@@ -31,32 +31,23 @@ public class CapApplication {
 		System.setProperty("java.awt.headless", "false");
 		log.info("Starting open Google Chrome in incognito mode");
 		// Set path to the ChromeDriver executable
-		System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
-		// Path to the Chrome extension (.crx file)
-		String extensionPath = "/Users/dabeeovina/Documents/MySelf/crawler-extension/extension.crx";
-
+//		System.setProperty("webdriver.chrome.driver", this.chromeDriverPath);
+		Path currentWorkingDir = Paths.get("").toAbsolutePath();
+		String rootDir = currentWorkingDir.getParent().toString();
+//		String extensionPath = rootDir + "/extension.crx";
 		// Path to the key file (.pem) if the extension is packed with a key
-		String keyPath = "/Users/dabeeovina/Documents/MySelf/crawler-extension/extension.pem";
+//		String keyPath = rootDir + "/extension.pem";
 
 		// Create ChromeOptions to enable incognito mode and open the console
-		ChromeOptions options = new ChromeOptions();
+//		ChromeOptions options = new ChromeOptions();
 //		options.addArguments("--incognito");
-		options.addExtensions(new File(extensionPath));
+//		options.addExtensions(new File(extensionPath));
 		// If the extension is packed with a key, add it as well
-		options.addArguments("--pack-extension-key=" + keyPath);
+//		options.addArguments("--pack-extension-key=" + keyPath);
 
 		// Initialize the ChromeDriver with the ChromeOptions
-		WebDriver driver = new ChromeDriver(options);
+//		WebDriver driver = new ChromeDriver(options);
 
-		driver.get("https://google.com/");
-		// Wait for a while to let DevTools load
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		// run python code to simulate key down
 //			Robot robot = new Robot();
 //
 //			// Simulate pressing Cmd + Option + J
@@ -67,29 +58,37 @@ public class CapApplication {
 //			robot.keyRelease(KeyEvent.VK_J);      // Release J
 //			robot.keyRelease(KeyEvent.VK_ALT);    // Release Option
 //			robot.keyRelease(KeyEvent.VK_META);
-			String pythonCmd = "python3 /Users/dabeeovina/Documents/MySelf/crawler-extension/autorun.py";
+		// run python code to simulate key down
+		String pythonCmd = "python3 " + rootDir + "/autorun.py";
 			boolean result = AppUtils.RunCmd(pythonCmd);
-			if (result) {
-//				driver.get("https://shopee.vn");
-//				this.autoLogin(driver);
-				// send a socket to FE to auto crawl starting
-			}
-//			Runtime.getRuntime().exec("python3 /Users/dabeeovina/Documents/MySelf/crawler-extension/autorun.py");
+//			if (result) {
+//				driver.get(this.shopeeLink);
+//				try {
+//					Thread.sleep(5000);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+////				driver.get("https://shopee.vn");
+////				this.autoLogin(driver);
+//				// send a socket to FE to auto crawl starting
+//			}
 
 		// Close the WebDriver
 //		driver.quit();
 	}
 
 	private void autoLogin(WebDriver driver) {
-		WebElement useremail = driver.findElement(By.xpath("//input[@placeholder='Email']"));
-		WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+		WebElement useremail = driver.findElement(By.xpath("//input[@name='loginKey']"));
+		WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+		useremail.sendKeys("dungzboo@gmail.com");
+		password.sendKeys("Vietdungzykh");
 
-		WebElement login = driver.findElement(By.xpath("//a[@class='btn-signin']"));
+		WebElement formElement = driver.findElement(By.cssSelector("div#main form"));
+		formElement.findElements(By.tagName("button")).get(1).click();
 
-		useremail.sendKeys("abc@mailinator.com"); password.sendKeys("XXX");
-		login.click(); String actualurl="url";
+//		plants.get(1).click();
 
-		String expectedurl= driver.getCurrentUrl();
+//		String expectedurl= driver.getCurrentUrl();
 //		Assert.assertEquals(expectedurl,actualurl);
 	}
 }
