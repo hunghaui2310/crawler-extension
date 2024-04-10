456f587438ab4b2f89c42ce5d56f5f0ef401141f
Hungnn <hung.nguyen@dabeeovina.com>
2024-04-10 23:29:25 +0700

    fix backup data

--- crawler/src/main/java/com/hk/max/concurrence/BackupTask.java
+++ crawler/src/main/java/com/hk/max/concurrence/BackupTask.java
@@ -2,6 +2,9 @@ package com.hk.max.concurrence;
 
 import com.hk.max.utils.AppUtils;
 import com.hk.max.utils.DateUtil;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
@@ -10,22 +13,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
 
 @Component
 @Slf4j
 public class BackupTask {
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
     @Value("${spring.app.backup_dir}")
     private String backupDir;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String URL_MONGDO;
 
     @PostConstruct
     public void backupDataAtStartup() {
@@ -33,36 +39,46 @@ public class BackupTask {
     }
 
     @PreDestroy
-    public void backupDataBeforeShutdown() {
+    public void backupDataBeforeShutdown() throws IOException, InterruptedException {
         this.backupData();
     }
 
     public void backupData() {
-        log.info("Called to backup data: " + this.backupDir);
-        Set<String> collectionNames = mongoTemplate.getCollectionNames();
+        try (MongoClient mongoClient = MongoClients.create(this.URL_MONGDO)) {
+            MongoDatabase database = mongoClient.getDatabase("crawler");
 
-        for (String collectionName : collectionNames) {
-            List<Object> collectionData = mongoTemplate.findAll(Object.class, collectionName);
+            // Get collection names
+            List<String> collectionNames = new ArrayList<>();
+            for (String name : database.listCollectionNames()) {
+                collectionNames.add(name);
+            }
 
-            writeCollectionDataToFile(collectionName, collectionData);
+            // Export each collection
+            for (String collectionName : collectionNames) {
+                MongoCollection<Document> collection = database.getCollection(collectionName);
+                exportCollection(collectionName, collection);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public void writeCollectionDataToFile(String collectionName, List<Object> collectionData) {
+    void exportCollection(String collectionName, MongoCollection<Document> collection) {
         String today = DateUtil.getCurrentTimeStamp(null);
         String dir = this.backupDir + "/backup/" + today;
         AppUtils.createFolderIfNotExist(dir);
-
-        String fileName = dir + "/" + collectionName + ".json";
-        try (FileWriter fileWriter = new FileWriter(fileName)) {
-            // Write collection data as JSON to the file
-            for (Object data : collectionData) {
-                fileWriter.write(data.toString());
-                fileWriter.write("\n");
+        String filePath = dir + "/" + collectionName + ".json";
+        // Perform export of collection data as needed
+        // For example, you can iterate through documents and write them to a file
+        // Here's just a simple example printing documents to the console
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            log.info("Exporting collection: " + collectionName);
+            for (Document document : collection.find()) {
+                fileWriter.write(document.toJson());
+                fileWriter.write("\n"); // Add newline after each document
             }
-            log.info("Exported collection '" + collectionName + "' to " + fileName);
         } catch (IOException e) {
-            log.error("Error occurred while writing collection '" + collectionName + "' to file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
