fad36f951a08ff0cb4ac4393d3f4228fb8a7003e
Hungnn <hung.nguyen@dabeeovina.com>
2024-04-10 23:29:25 +0700

    fix backup data

--- crawler/src/main/java/com/hk/max/cron/BackupDataCron.java
+++ crawler/src/main/java/com/hk/max/cron/BackupDataCron.java
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class BackupDataCron {
 
     @Autowired
     private BackupTask backupTask;
 
-    @Scheduled(cron = "0 0 3 * * *")
-    public void backupDataInEveryDay() {
-        this.backupTask.backupData();
-    }
+//    @Scheduled(cron = "0 0 3 * * *")
+//    public void backupDataInEveryDay() throws IOException, InterruptedException {
+//        this.backupTask.backupData();
+//    }
 }
